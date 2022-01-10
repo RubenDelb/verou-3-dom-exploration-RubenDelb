4bd75ddccd1cff5c48c2f4c82c5b0d55f6642f0c
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-10 10:36:05 +0100

    Add random generated RGB background color

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log(document.title);
 document.title = "Modifying the DOM";
 
 console.log(document.title);
+
+function random_rgb() {
+    var a = Math.round, b = Math.random, c = 255;
+    return 'rgb(' + a(b()*c) + ',' + a(b()*c) + ',' + a(b()*c) + ')';
+}
+
+var color = random_rgb()
+
+document.body.style.backgroundColor = color;
