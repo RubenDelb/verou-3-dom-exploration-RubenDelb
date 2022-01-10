6edd07ed52f7d195afeb83fb9b666de948b8cdc4
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-10 11:31:12 +0100

    adding comments to the script

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+//show document title in the console
 console.log(document.title);
 
+//change the document title
 document.title = "Modifying the DOM";
 
+//show the new document title
 console.log(document.title);
 
+//add a random rgb background color when the page loads
 function random_rgb() {
     var a = Math.round, b = Math.random, c = 255;
     return 'rgb(' + a(b()*c) + ',' + a(b()*c) + ',' + a(b()*c) + ')';
 }
 
-var randomColor = random_rgb()
+document.body.style.backgroundColor = random_rgb();
 
-document.body.style.backgroundColor = randomColor;
-
+//show all the child elements of the body in the console
 var d = document.body.children;
 for (const element of d) {
     console.log(element);
