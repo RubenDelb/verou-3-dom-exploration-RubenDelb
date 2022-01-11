c260d60c3ee170d0fdc6da9b20de187bcbed967a
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-11 14:45:37 +0100

    Add random bgcolor to every section and change textcolor(black or white) to corresponding background

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -2,14 +2,32 @@ const names = ["Pieter", "Dery", "Ahmad", "Jian", "Michael", "Dante", "Muhammet"
 
 const article = document.querySelector("article");
 
+function random_rgb() {
+    var a = Math.round,
+        b = Math.random,
+        c = 255;
+    return 'rgb(' + a(b() * c) + ',' + a(b() * c) + ',' + a(b() * c) + ')';
+}
+
+function BlackOrWhite(rgb) {
+    rgb = rgb.match(/\d+/g);
+    if ((rgb[0] * 0.299) + (rgb[1] * 0.587) + (rgb[2] * 0.114) > 128) {
+        return 'black';
+    } else {
+        return 'white';
+    }
+}
+
 for (let oneName of names) {
-    console.log(names[oneName])
+    console.log(oneName);
     const newSection = document.createElement("section");
+    newSection.style.backgroundColor = random_rgb();
+    newSection.style.color = BlackOrWhite(newSection.style.backgroundColor);
     const newParagraph = document.createElement("p");
     newParagraph.innerHTML = oneName;
     newSection.appendChild(newParagraph);
     article.appendChild(newSection);
-} 
+}
 
 
 
