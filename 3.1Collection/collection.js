0345a522965cbffb30db4d01b48f7f310081bf9f
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-14 10:20:38 +0100

    FINAL pathname-fix and it works on netlify now

--- 3.1Collection/collection.js
+++ 3.1Collection/collection.js
@@ -1,6 +1,6 @@
 const recipes = [{
         delink: 'https://dagelijksekost.een.be/gerechten/chili-no-carne',
-        picture: '/3.1Collection/Pictures/ChiliSinCarne.jpg',
+        picture: './Pictures/ChiliSinCarne.jpg',
         name: 'Chili Sin Carne',
         description: 'This flavorful dish is ideal for a quick stew on busy weeknights!',
         origin: 'Mexico',
@@ -9,7 +9,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.indianhealthyrecipes.com/baingan-bharta/',
-        picture: '/3.1Collection/Pictures/BainganBartha.jpg',
+        picture: './Pictures/BainganBartha.jpg',
         name: 'Baingan Bhartha',
         description: 'Baingan bharta is a divine mash of smoky aubergines, mixed with Indian seasonings. Delicious for dipping with bread.',
         origin: 'India',
@@ -18,7 +18,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.libelle-lekker.be/bekijk-recept/18130/paprikasoep-met-mascarpone-en-pesto',
-        picture: '/3.1Collection/Pictures/paprikapestomascarponesoep.jpg',
+        picture: './Pictures/paprikapestomascarponesoep.jpg',
         name: 'Pepper-pesto soup',
         description: 'A rich and intensely flavoured roasted red pepper soup made with juicy and charred yellow peppers & mascarpone.',
         origin: 'Italy',
@@ -27,7 +27,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.feastingathome.com/vegetarian-shepherds-pie/',
-        picture: '/3.1Collection/Pictures/IndianShepherdsPie.jpg',
+        picture: './Pictures/IndianShepherdsPie.jpg',
         name: 'Indian Shepherds Pie',
         description: 'A vegetarian Shepherd\'s Pie infused with the most fragrant Indian spices.',
         origin: 'India',
@@ -36,7 +36,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.leukerecepten.nl/recepten/shakshuka/',
-        picture: '/3.1Collection/Pictures/Shakshuka.jpg',
+        picture: './Pictures/Shakshuka.jpg',
         name: 'Shakshuka',
         description: 'Shakshuka is a North African and Middle Eastern meal of poached eggs in a simmering tomato sauce with spices.',
         origin: 'Tunisia',
@@ -45,7 +45,7 @@ const recipes = [{
     },
     {
         delink: 'https://cookieandkate.com/thai-panang-curry-recipe/',
-        picture: '/3.1Collection/Pictures/PanaengCurry.jpg',
+        picture: './Pictures/PanaengCurry.jpg',
         name: 'Penaeng curry',
         description: 'It\'s rich and spicy with a distinctly unique flavor from the addition of kaffir lime leaves and panaeng curry paste.',
         origin: 'Thailand',
@@ -54,7 +54,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.healthyfood.com/healthy-recipes/vegetarian-tagine/',
-        picture: '/3.1Collection/Pictures/Tagine.jpg',
+        picture: './Pictures/Tagine.jpg',
         name: 'Vegetarian Tagine',
         description: 'This full-flavoured tagine is a cheap, healthy way of getting extra vegetables into your family\'s diet. ',
         origin: 'Morocco',
@@ -63,7 +63,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.veguru.be/recipe/vegan-stoverij/',
-        picture: '/3.1Collection/Pictures/vegan-stew.jpg',
+        picture: './Pictures/vegan-stew.jpg',
         name: 'Vegan Stew',
         description: 'A delicious stew with fries always tastes great! This Flemish classic is easy to make without meat.',
         origin: 'Belgium',
@@ -72,7 +72,7 @@ const recipes = [{
     },
     {
         delink: 'https://chickslovefood.com/recept/pad-thai-met-tofu/',
-        picture: '/3.1Collection/Pictures/PadThai.jpg',
+        picture: './Pictures/PadThai.jpg',
         name: 'Pad Thai',
         description: 'It\'s a stir-fried rice noodle dish commonly served as a street food in Thailand as part of the country\'s cuisine.',
         origin: 'Thailand',
@@ -81,7 +81,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.mygingergarlickitchen.com/jaipuri-aloo-pyaaz-ki-sabzi-potato-onion-curry-video-recipe/',
-        picture: '/3.1Collection/Pictures/JaipuriAloo.jpg',
+        picture: './Pictures/JaipuriAloo.jpg',
         name: 'Jaipuri Aloo',
         description: 'A heavenly preparation using potatoes and onions, cooked in a scrumptious curry, packed with savoury flavors',
         origin: 'India',
@@ -90,7 +90,7 @@ const recipes = [{
     },
     {
         delink: 'https://www.hellofresh.be/recipes/linguine-met-cherrytomaten-uit-de-oven-5be18ffeae08b5275a1375f2',
-        picture: '/3.1Collection/Pictures/LinguineCherrytomaten.jpg',
+        picture: './Pictures/LinguineCherrytomaten.jpg',
         name: 'Cherrytom. Linguine',
         description: 'Juicy cherry tomatoes that burst open in warm olive oil, creating a luxurious sauce that comes together in minutes.',
         origin: 'Italy',
@@ -99,14 +99,14 @@ const recipes = [{
     },
     {
         delink: 'https://www.libelle-lekker.be/bekijk-recept/84072/veganistische-vol-au-vent-1',
-        picture: '/3.1Collection/Pictures/vol-au-vegan.jpg',
+        picture: './Pictures/vol-au-vegan.jpg',
         name: 'Vol-Au-Vegan',
         description: 'An extremely delicious vegan version of the classic vol-au-vent.',
         origin: 'Belgium',
         preparationTime: '45min',
         difficulty: 'Medium',
     },
-    
+
 ]
 
 export {
