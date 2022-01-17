47dc26e2f69473caad2d048495bdb33bf34ca469
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-17 16:42:57 +0100

    finished exercise except last one (alertbox)

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -5,9 +5,10 @@ ol.insertBefore(lastListItem, firstListItem); //Insert the last child as the fir
 
 const secondSection = document.querySelectorAll("section")[1]; //select the second section
 const thirdSection = document.querySelectorAll("section")[2]; //select the third section
+const div = document.querySelector("div");
 const thirdHeader = secondSection.children[0]; //set variable for the header that should be in the third section
 const secondHeader = thirdSection.children[0].children[0]; //set variable for the header that should be in the second section
 secondSection.insertBefore(secondHeader, secondSection.children[0]); //append the second header to the second section
-thirdSection.insertBefore(thirdHeader, thirdSection.children[0]); //append the third header to the third section
+div.insertBefore(thirdHeader, div.children[0]); //append the third header to the third section
 
 thirdSection.remove(); //remove the third section of the DOM
--- 5.Events/script.js
+++ 5.Events/script.js
@@ -4,12 +4,66 @@ function getElapsedTime() {
     return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+const list = document.querySelector("ul");
+
 function clickOnSquare(e) {
     console.log(e.target.classList[1])
     console.log(getElapsedTime())
+    const wrapper = document.querySelector(".displayedsquare-wrapper");
+    const newDiv = document.createElement("div");
+    newDiv.className = "displayedsquare" + " " + e.target.classList[1];
+    wrapper.appendChild(newDiv);
+    const newListItem = document.createElement("li");
+    newListItem.innerHTML = "[" + getElapsedTime() + "]" + " Created a new " + e.target.classList[1] + " square.";
+    list.append(newListItem);
 }
 
 const actionsquares = document.querySelectorAll('.actionsquare')
+
 for (let actionsquare of actionsquares) {
     actionsquare.addEventListener('click', clickOnSquare)
+}
+
+function random_rgb() {
+    var a = Math.round,
+        b = Math.random,
+        c = 255;
+    return 'rgb(' + a(b() * c) + ',' + a(b() * c) + ',' + a(b() * c) + ')';
+}
+
+document.body.onkeyup = function (e) {
+    if (e.keyCode == 32) {
+        document.body.style.backgroundColor = random_rgb();
+        const newListItem = document.createElement("li");
+        newListItem.innerHTML = "[" + getElapsedTime() + "]" + " Spacebar has been smashed!";
+        list.append(newListItem);
+    }
+    const listItems = document.querySelector("ul").children;
+    if (e.keyCode == 76) {
+        for (let i = listItems.length - 1; i >= 0; --i) {
+            listItems[i].remove();
+        }
+    }
+    const newSquares = document.querySelector(".displayedsquare-wrapper").children;
+    if (e.keyCode == 83) {
+        for (let i = newSquares.length - 1; i >= 0; --i) {
+            newSquares[i].remove();
+        }
+    }
+}
+
+// const newSquares = document.querySelector(".displayedsquare-wrapper").children;
+
+// newSquares.addEventListener("click", function () {
+//     alert("Hello World!");
+// })
+
+const displayedsquares = document.querySelectorAll('.displayedsquare')
+
+function alertMessage() {
+    console.log("hey");
+    alert("Hello World!");} 
+
+for (let displayedsquare of displayedsquares) {
+    displayedsquare.addEventListener('click', alertMessage)
 }
