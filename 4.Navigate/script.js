f3fb61f7343f40b238ae078ed48423b1c6c5e563
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-01-17 11:24:51 +0100

    Modified the DOM as explained in the exercise

---
+++ 4.Navigate/script.js
@@ -0,0 +1,13 @@
+const ol = document.querySelector('ol'); //select the ordered list in the DOM
+const lastListItem = ol.children[4]; //set variable for the last child of the list
+const firstListItem = ol.children[0]; //set variable for the first child of the list
+ol.insertBefore(lastListItem, firstListItem); //Insert the last child as the first one in the list
+
+const secondSection = document.querySelectorAll("section")[1]; //select the second section
+const thirdSection = document.querySelectorAll("section")[2]; //select the third section
+const thirdHeader = secondSection.children[0]; //set variable for the header that should be in the third section
+const secondHeader = thirdSection.children[0].children[0]; //set variable for the header that should be in the second section
+secondSection.insertBefore(secondHeader, secondSection.children[0]);
+thirdSection.insertBefore(thirdHeader, thirdSection.children[0]);
+
+thirdSection.remove();
